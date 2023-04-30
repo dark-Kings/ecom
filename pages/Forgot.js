89233b3561eade833e73b23e7945e2dd30a470a1
Anshul Pant <anshulpant14@gmail.com>
2023-05-01 00:56:51 +0530

    30 commits-5

--- pages/Forgot.js
+++ pages/Forgot.js
@@ -1,25 +1,190 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {AiOutlineLock } from 'react-icons/ai';
 import Link from 'next/link';
 import Head from 'next/head';
+import emailjs from "@emailjs/browser";
 import { useRouter } from 'next/router';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Forgot = () => {
   const router = useRouter();
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [cpassword, setCpassword] = useState('')
+  const [loading, setLoading] = useState(false);
+
+
+
   useEffect(() => {
     if(localStorage.getItem('token')){
       router.push('/')
     }
    }, [])
+
+
+
+   const handleChange = async (e) => {
+
+
+    if (e.target.name == 'email') {
+      setEmail(e.target.value)
+    }
+    else if (e.target.name == 'password') {
+      setPassword(e.target.value)
+    }
+    else if (e.target.name == 'cpassword') {
+      setCpassword(e.target.value)
+    }
+  }
+
+ const sendResetEmail= async (e)=>{
+  let data = { email, sendMail:true}
+  const a = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/forgot`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  })
+
+  let res = await a.json();
+  
+  if(res.success){
+  e.preventDefault();
+  setLoading(true);
+
+
+  emailjs.send(
+    // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+    'service_ja43e7i',
+    // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+    'template_bcwomjs',
+    {
+      from_name: res.name,
+      to_name: "Anshul",
+      from_email: email,
+      to_email: "anshulpant14@gmail.com",
+      message: res.emails,
+    },
+    // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    'NbhAYb4ujSinSanZ5'
+  )
+
+    .then(
+      () => {
+        setLoading(false);
+        toast.success("Password reset instruction have been sent to your email", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      },
+      (error) => {
+        setLoading(false);
+        // console.error(error);
+
+        toast.error("something went wrong try again", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    );
+  
+  
+  }
+  else{
+    toast.error("something went wrong try again", {
+      position: "bottom-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+ }
+
+
+ const resetPassword= async ()=>{
+  const tokens= router.query.token
+
+  if(password==cpassword){
+  let data = { tokens,password, sendMail:false}
+  const a = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/forgot`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  })
+
+  let res = await a.json();
+  if(res.success){
+    toast.success("Password has been changed", {
+      position: "bottom-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+     setTimeout(() => {
+      router.push('/Login')
+     }, [800]);
+  }
+  else{
+    toast.error("something went wrong try again", {
+      position: "bottom-left",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+}
+ }
+
+
   return (
     <>
       <Head>
         <title>Forget -codeswear.com</title>
         </Head>
+        <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     <div className="min-h-screen flex items-start  justify-center py-24 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-            {router.query.token}
+           
             <img
               className="mx-auto h-20 w-auto"
               src="/logo.jpg"
@@ -35,8 +200,63 @@ const Forgot = () => {
               </Link>
             </p>
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" defaultValue="true" />
+          {router.query.token  && <div>
+       
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  onChange={handleChange}
+                  name="password"
+                  type="password"
+                  autoComplete="password"
+                  value={password}
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+              <div>
+                <label htmlFor="cpassword" className="sr-only">
+                 Confirm Password
+                </label>
+                <input
+                  id="cpassword"
+                  onChange={handleChange}
+                  name="cpassword"
+                  type="password"
+                  value={cpassword}
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
+                  placeholder="Confirm password"
+                />
+              </div>
+      
+            </div>
+            <div>
+              <button
+             
+                onClick={resetPassword}
+                type="submit"
+                className="group relative my-4 flex w-full justify-center rounded-md border border-transparent bg-pink-600 py-2 px-4 text-sm font-medium text-white hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <AiOutlineLock className="h-5 w-5 text-pink-500 group-hover:text-pink-400"/>
+                </span>
+                Change Password
+              </button>
+            </div>
+          {password != cpassword &&
+            <span className='text-red-600'>Password don't matched</span>}
+         {password == cpassword && password &&
+            <span className='text-green-600'>Password  matched</span>}
+
+          </div>}
+         {!router.query.token && <div>
             <div className="-space-y-px rounded-md shadow-sm">
               <div>
                 <label htmlFor="email-address" className="sr-only">
@@ -44,9 +264,11 @@ const Forgot = () => {
                 </label>
                 <input
                   id="email-address"
+                  onChange={handleChange}
                   name="email"
                   type="email"
                   autoComplete="email"
+                  value={email}
                   required
                   className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm"
                   placeholder="Email address"
@@ -54,21 +276,20 @@ const Forgot = () => {
               </div>
       
             </div>
-
-          
-
             <div>
               <button
+              onClick={sendResetEmail}
                 type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-pink-600 py-2 px-4 text-sm font-medium text-white hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+                className="group relative my-4 flex w-full justify-center rounded-md border border-transparent bg-pink-600 py-2 px-4 text-sm font-medium text-white hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <AiOutlineLock className="h-5 w-5 text-pink-500 group-hover:text-pink-400"/>
                 </span>
-                Continue
+                Change Password
               </button>
             </div>
-          </form>
+          </div>
+          }
         </div>
       </div>
     </>
--- pages/api/forgot.js
+++ pages/api/forgot.js
@@ -1,28 +1,51 @@
 import Forgot from "../../models/Forgot"
-
+import User from "../../models/User"
 import connectDb from '../../middleware/mongoose';
+var CryptoJS = require("crypto-js");
 
 
-const  handler = async (req, res)=>{
-   
-    //Check if the user exists in the DB
 
-    let token = `aljl;aj;lajdfkldjljfdslfjkdjjsflafjal;`
-    let forgot = new Forgot({email:req.body.email})
+const handler = async (req, res) => {
 
 
-    let email =   `We have sent you this email in response to your request to reset your password on CodesWear.com.
 
+    if (req.body.sendMail) {
+        //Check if the user exists in the DB
+        let tempUser = await User.findOne({ email: req.body.email })
+       
+        if (tempUser) {
+            let token = Math.floor(Math.random() * Date.now())
+            let Lforgot = new Forgot({email:req.body.email,token})
+            await Lforgot.save()
+
+
+            let emails = `We have sent you this email in response to your request to reset your password on CodesWear.com.
     To reset your password, please follow the link below:
-
-    <a href="http//codeswear.com/Forgot?token=${token}">Click here to reset your Password</a>
-
-   
-
+    <a href="http://localhost:3000//Forgot?token=${token}">Click here to reset your Password</a>
     We recommend that you keep your password secure and not share it with anyone.If you feel your password has been compromised, you can change it by going to your  My Account Page and change your Password `
 
-    res.status(200).json({ name: 'John Doe' })
-  }
+    res.status(200).json({ success: true,emails,name:tempUser.name })
 
-  export default connectDb(handler)
-  
+        }
+        else {
+            res.status(200).json({ success: false })
+        }
+    }
+    else {
+        // Reset User Password
+        const temp2user = await Forgot.findOne({token:req.body.tokens})
+        if(temp2user){
+        await User.findOneAndUpdate({ email: temp2user.email }, { password: CryptoJS.AES.encrypt(req.body.password, `${process.env.NEXT_PUBLIC_AES_SECRET}`).toString() })
+        await Forgot.findOneAndDelete({token:req.body.tokens})
+        res.status(200).json({ success: true })
+        return
+        }
+        else{
+        res.status(200).json({ success: false })
+
+        }
+    }
+
+}
+
+export default connectDb(handler)
