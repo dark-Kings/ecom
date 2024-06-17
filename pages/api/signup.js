f0aed7f90102ecaf0fcc4cf45c0d537e64ca342c
Arya <arya@mr.hakninja@gmail.com>
2024-06-17 11:58:52 +0530

    set payment gateway

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -5,6 +5,7 @@ var CryptoJS = require("crypto-js");
 const handler = async (req, res) => {
   if (req.method == 'POST') {
     const {name,email} = req.body;
+    console.log(req.body)
     let u = new User({name,email,password:CryptoJS.AES.encrypt(req.body.password,`${process.env.NEXT_PUBLIC_AES_SECRET}`).toString()});
     await  u.save();
     res.status(200).json({ "success": true })
